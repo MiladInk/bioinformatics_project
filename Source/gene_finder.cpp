7cbd179f6a53274638613c30bd6df7bf32bfb4bc
milad <mi.aghajohari@gmail.com>
2018-01-26 01:34:53 +0330

    2.2

--- Source/gene_finder.cpp
+++ Source/gene_finder.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
-#include<string>
-#include<string.h>
+#include <iostream>
+#include <string>
+#include <string.h>
 #include <unistd.h>
-#include<fstream>
-#include<vector>
-#include<algorithm>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <sys/wait.h>
+#define MAX_GENE_LENGTH 20000 + 10
 #define fi first
 #define se second
 #define pb push_back
@@ -15,28 +17,98 @@ using namespace std;
   so we can find out the genes in the different type of viruses. 
 */
 string names[5] = {"Reston", "Sudan", "TaiForest", "Zaire", "Bundibugyo"};
-vector<pair<string, string> > marburg_genes;// here we store the murburg's genes first:gene_name second gene_string
-int main(){
+vector<pair<string, string>> marburg_genes; // here we store the murburg's genes first:gene_name second gene_string
+vector<pair<string, string>> ebola_genomes;
+int main()
+{
     ifstream marburg;
     marburg.open("../Data/Marburg_Genes.fasta");
     string inp;
-    while(marburg >> inp){
-        if(inp[0] == '>'){
+    while (marburg >> inp)
+    {
+        if (inp[0] == '>')
+        {
             inp = inp.substr(1);
             marburg_genes.pb(mp(inp, ""));
             continue;
         }
         marburg_genes.back().se = marburg_genes.back().se + inp;
-    } 
+    }
+    //printing the maruburg_genes
+    for (auto marburg_gene : marburg_genes)
+    {
+        //cout << marburg_gene.fi << " : " << marburg_gene.se << endl;
+        for (int i = 0; i < strlen(marburg_gene.se.c_str()); i++)
+            if (marburg_gene.se[i] == '\n')
+                cout << i << " : " << strlen(marburg_gene.se.c_str()) << endl;
+    }
     /* now we have read the marburg's genes. we are going to read the genome of other types
     making a file for their genes*/
     ifstream fin_genome;
     //looping over multiple ebola's type genes
-    for(auto name: names){
+    for (auto name : names)
+    {
+        ebola_genomes.pb(mp(name, ""));
         string file_name = "../Data/" + name + "_genome.fasta";
         fin_genome.open(file_name);
-        string text;
-        while(fin_genome >> text) cout << text << endl;
+        getline(fin_genome, inp); //here is the name of the type pf the eboal virus, going to trash
+        while (fin_genome >> inp)
+        {
+            ebola_genomes.back().se = ebola_genomes.back().se + inp;
+        }
         fin_genome.close();
     }
+    //printing the genomes
+    for (auto ebola_genome : ebola_genomes)
+    {
+        //cout << ebola_genome.fi << " : " << ebola_genome.se << endl;
+        for (int i = 0; i < strlen(ebola_genome.se.c_str()); i++)
+            if (ebola_genome.se.c_str()[i] == '\n')
+                cout << i << " : " << strlen(ebola_genome.se.c_str()) << endl;
+    }
+    int pipes[2][2];
+    char *semi_local[] = {"semi_local_align", 0};
+    int count = 0;
+    for (auto ebola_genome : ebola_genomes)
+    {
+        ofstream fout;
+        string out_name = "../Data/" + ebola_genome.fi + "_genes.fasta";
+        fout.open(out_name.c_str());
+        for (auto marburg_gene : marburg_genes)
+        {
+            cout << count++ << endl;
+            pipe(pipes[0]);
+            pipe(pipes[1]);
+            int cpid = 0;
+            if (!(cpid == fork()))
+            {
+                dup2(pipes[0][0], STDIN_FILENO);
+                dup2(pipes[1][1], STDOUT_FILENO);
+                close(pipes[0][0]);
+                close(pipes[0][1]);
+                close(pipes[1][0]);
+                close(pipes[1][1]);
+                execv(semi_local[0], semi_local);
+                return 0;
+            }
+            else
+            {
+                close(pipes[0][0]);
+                close(pipes[1][1]);
+                write(pipes[0][1], (void *)ebola_genome.se.c_str(), ebola_genome.se.length());
+                write(pipes[0][1], "\n", 1);
+                write(pipes[0][1], (void *)marburg_gene.se.c_str(), marburg_gene.se.length());
+                write(pipes[0][1], "\n", 1);
+                wait(NULL);
+                char buffer[MAX_GENE_LENGTH];
+                memset(buffer, 0, MAX_GENE_LENGTH);
+                read(pipes[1][0], buffer, MAX_GENE_LENGTH);
+                fout << ">" << marburg_gene.fi << endl;
+                fout << buffer << endl;
+            }
+        }
+        fout.close();
+    }
+    cout << "ended" << endl;
+    return 0;
 }
--- Source/semi_local_align.cpp
+++ Source/semi_local_align.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-#include<string>
-#include<string.h>
-#include<cmath>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <cmath>
+#include <algorithm>
 #define mp make_pair
 #define fi first
 #define se second
 using namespace std;
-string genome;//here we will store the refrence genome
-string gene;//here we will store the gene we are going to find in the refrence genome
+string genome; //here we will store the refrence genome
+string gene;   //here we will store the gene we are going to find in the refrence genome
 bool verbose = false;
-const int MAX_GENOME_LENGTH = 20000+10;
-const int MAX_GENE_LENGTH = 10000+10;
+const int MAX_GENOME_LENGTH = 20000 + 10;
+const int MAX_GENE_LENGTH = 10000 + 10;
 const int INF = int(1e9);
 const int GAP_COST = -1;
 const int DIF_VAL = -1;
@@ -29,13 +29,15 @@ pair<int, int> history[MAX_GENOME_LENGTH][MAX_GENE_LENGTH];
     handle i<0 && j<0 accesses more comfortable and not hard code it
     in every access
 */
-int get_dp(int i, int j){
-    if(i<0 || j<0)
-        return -1*INF;
+int get_dp(int i, int j)
+{
+    if (i < 0 || j < 0)
+        return -1 * INF;
     return score[i][j];
 }
 
-pair<int, int> get_mapped_range_in_genome(){
+pair<int, int> get_mapped_range_in_genome()
+{
     int max_i = 0;
     /*
         find the end of the mapped region so
@@ -43,8 +45,8 @@ pair<int, int> get_mapped_range_in_genome(){
         we handled dropping a part from the first of genome in code before
         but now we handle dropping a part from the end of genome
     */
-    for(int i = 0; i<genome.length()+1; i++)
-        if(score[i][gene.length()]>score[max_i][gene.length()])
+    for (int i = 0; i < genome.length() + 1; i++)
+        if (score[i][gene.length()] > score[max_i][gene.length()])
             max_i = i;
     /*      but what is the first of the mapped region?
         we will use history part of our program to remebmer this.
@@ -52,7 +54,8 @@ pair<int, int> get_mapped_range_in_genome(){
     */
     pair<int, int> var = mp(max_i, gene.length());
     // cout <<"history began" << endl;
-    while(var.se != 0){
+    while (var.se != 0)
+    {
         /*
         log the history for debug
         */
@@ -62,56 +65,64 @@ pair<int, int> get_mapped_range_in_genome(){
     }
     // cout << var.fi <<" "<<var.se << endl;
     // cout << "history ended" << endl;
-    return mp(var.fi+1, max_i);
+    return mp(var.fi + 1, max_i);
 }
 
-int main(int argc, const char**argv){
-    /*input format: genome, gene*/    
+int main(int argc, const char **argv)
+{
+    /*input format: genome, gene*/
     cin >> genome >> gene;
     /*check if the limits are not reached*/
-    if(genome.length() > MAX_GENOME_LENGTH)
+    if (genome.length() > MAX_GENOME_LENGTH)
         cout << "MAX_GENOME_LENGTH is exceeded." << endl;
-    if(gene.length() > MAX_GENE_LENGTH)
+    if (gene.length() > MAX_GENE_LENGTH)
         cout << "MAX_GENE_LENGTH is exceeded." << endl;
     /*should be verbose?*/
-    for(int i = 0; i<argc; i++){
-        if(strcmp(argv[i], "-verbose")==0){
-         verbose = true;
+    for (int i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-verbose") == 0)
+        {
+            verbose = true;
         }
     }
 
-    if(verbose == true){
-    cout <<"refrence genome is : "<< genome << endl;
-    cout <<"gene : " << gene << endl;
+    if (verbose == true)
+    {
+        cout << "refrence genome is : " << genome << endl;
+        cout << "gene : " << gene << endl;
     }
 
     /* initialize some variables, it seems it sufficies to fill
        just the first row and first column*/
     score[0][0] = 0;
     history[0][0] = mp(0, 0);
-    for(int i = 0; i<genome.length()+1; i++){
+    for (int i = 0; i < genome.length() + 1; i++)
+    {
         score[i][0] = 0;
         history[i][0] = mp(i, 0);
     }
-    for(int i = 1; i<gene.length()+1; i++){
-        score[0][i] = GAP_COST*i;
-        history[0][i] = mp(0, i-1);
+    for (int i = 1; i < gene.length() + 1; i++)
+    {
+        score[0][i] = GAP_COST * i;
+        history[0][i] = mp(0, i - 1);
     }
     /*we are going to fill dynamic programming array here*/
-    
-    for(int i = 1; i<genome.length()+1; i++){
-        for(int j = 1; j<gene.length()+1; j++){
+    for (int i = 1; i < genome.length() + 1; i++)
+    {
+        for (int j = 1; j < gene.length() + 1; j++)
+        {
             /*
                 here we check making GAPS, just making a gap and
                 then continue, this can be good in many cases but we
                 will take care of not penalty the dp for dropping the first part of 
                 genome at the end of this loop 
             */
-            score[i][j] = get_dp(i-1, j) + GAP_COST;
-            history[i][j] = mp(i-1, j);
-            if(score[i][j] < get_dp(i, j-1)+GAP_COST){
-                score[i][j] = get_dp(i, j-1)+GAP_COST;
-                history[i][j] = mp(i, j-1);
+            score[i][j] = get_dp(i - 1, j) + GAP_COST;
+            history[i][j] = mp(i - 1, j);
+            if (score[i][j] < get_dp(i, j - 1) + GAP_COST)
+            {
+                score[i][j] = get_dp(i, j - 1) + GAP_COST;
+                history[i][j] = mp(i, j - 1);
             }
             /*
             if we are going to map these two character together
@@ -121,15 +132,20 @@ int main(int argc, const char**argv){
             if DIF_VAL is or small it seems better to map the unmatching characters instead of 
             just making two gaps
             */
-            if(genome[i-1] == gene[j-1]){
-                if(score[i][j] < get_dp(i-1, j-1) + EQ_VAL){
-                    score[i][j] = get_dp(i-1, j-1) + EQ_VAL;
-                    history[i][j] = mp(i-1, j-1);
+            if (genome[i - 1] == gene[j - 1])
+            {
+                if (score[i][j] < get_dp(i - 1, j - 1) + EQ_VAL)
+                {
+                    score[i][j] = get_dp(i - 1, j - 1) + EQ_VAL;
+                    history[i][j] = mp(i - 1, j - 1);
                 }
-            }else{
-                if(score[i][j] < get_dp(i-1, j-1)+DIF_VAL){
-                    score[i][j] = get_dp(i-1, j-1)+DIF_VAL;
-                    history[i][j] = mp(i-1, j-1);
+            }
+            else
+            {
+                if (score[i][j] < get_dp(i - 1, j - 1) + DIF_VAL)
+                {
+                    score[i][j] = get_dp(i - 1, j - 1) + DIF_VAL;
+                    history[i][j] = mp(i - 1, j - 1);
                 }
             }
         }
@@ -145,8 +161,8 @@ int main(int argc, const char**argv){
             similiar to given gene
     */
     pair<int, int> ans = get_mapped_range_in_genome();
-    cout <<"Index "<< ans.fi << " to " << ans.se <<
-    " of the genome with score " << score[ans.se][gene.length()]<< endl;
-    cout << "The found string is :" << endl;
-    cout << genome.substr(ans.fi-1, ans.se-ans.fi+1) << endl; 
+    // cout <<"Index "<< ans.fi << " to " << ans.se <<
+    // " of the genome with score " << score[ans.se][gene.length()]<< endl;
+    // cout << "The found string is :" << endl;
+    cout << genome.substr(ans.fi - 1, ans.se - ans.fi + 1) << endl;
 }
