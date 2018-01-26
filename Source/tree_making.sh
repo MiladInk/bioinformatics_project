0cb2cd950b8cea51a0c92ec7ac8a9964c8b1b6d4
milad <mi.aghajohari@gmail.com>
2018-01-27 01:09:15 +0330

    photo of tree

--- Source/tree_making.sh
+++ Source/tree_making.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+rm ../Data/Trees/UPGMA.trees
+rm ../Data/Trees/NJ.trees
 for filename in ../Data/Dists/*.csv; do
      name="$(basename "$filename")"
      name=${name%.*}
      chmod +x tree_drawer.py
-     ./UPGMA < "$filename" | ./tree_drawer.py >../Data/Trees/"$name"_UPGMA.tree
-     ./Neighbour_Joining < "$filename" | ./tree_drawer.py >../Data/Trees/"$name"_Neighbour_Joining.tree
+     path1="../Data/Trees/"$name"_UPGMA.png\n"
+     { ./UPGMA < "$filename";echo $path1; } | ./tree_drawer.py 
+     ./UPGMA < "$filename" >> ../Data/Trees/UPGMA.trees
+     path="../Data/Trees/"$name"_NJ.png\n"
+     { ./Neighbour_Joining < "$filename";echo $path; } | ./tree_drawer.py 
+     ./Neighbour_Joining < "$filename" >> ../Data/Trees/NJ.trees
+     
 done
