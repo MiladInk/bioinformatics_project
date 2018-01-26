4fb2d84381a4d33e4d381c00f82b9497233a2f9b
milad <mi.aghajohari@gmail.com>
2018-01-27 01:09:25 +0330

    drawer

--- Source/tree_drawer.py
+++ Source/tree_drawer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 from Bio import Phylo
 from io import StringIO
+import matplotlib.pyplot as plt
 str = input()
+pat = "salam"
+try:
+    pat = input()
+except EOFError:
+    a = 10
 inp = StringIO(str)
 tree = Phylo.read(inp, "newick")
-Phylo.draw_ascii(tree)
+Phylo.draw(tree, do_show=False)
+plt.savefig(pat)
